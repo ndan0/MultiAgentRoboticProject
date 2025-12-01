0b29db9df9167605c444869df24f0d1b94988a50
Dan Nguyen <ndan0112@gmail.com>
2025-12-01 02:45:01 -0600

    Smart indent and code formatted

--- applyRule1.m
+++ applyRule1.m
@@ -1,44 +1,44 @@
 function [A, nodes] = applyRule1(A, nodes)
-    N = size(A,1);
+N = size(A,1);
 
-    % Find all unused node (circle node)
-    circleNodeIndexes = [];
-    for i = 1:N
-        if isequal(nodes{i}.shape, 'o')
-            circleNodeIndexes = [circleNodeIndexes; i];
+% Find all unused node (circle node)
+circleNodeIndexes = [];
+for i = 1:N
+    if isequal(nodes{i}.shape, 'o')
+        circleNodeIndexes = [circleNodeIndexes; i];
+    end
+end
+
+% Chose random node
+if length(circleNodeIndexes) <=0
+    return; % Exit if no circle nodes are found
+end
+newNodeIndex = circleNodeIndexes(randi(length(circleNodeIndexes)));
+
+% Find all edge of A in from of (nodea, nodeb)
+edges = []; % Initialize empty list
+
+for i = 1:N
+    for j = i+1:N  % check only upper triangle
+        if A(i,j) ~= 0
+            edges = [edges; i, j];
         end
     end
-    
-    % Chose random node
-    if length(circleNodeIndexes) <=0
-        return; % Exit if no circle nodes are found
-    end
-    newNodeIndex = circleNodeIndexes(randi(length(circleNodeIndexes)));
+end
 
-    % Find all edge of A in from of (nodea, nodeb)
-    edges = []; % Initialize empty list
+% Chose a random edge
+idx = randi(size(edges,1));
+e = edges(idx, :);
+a = e(1);
+b = e(2);
 
-    for i = 1:N
-        for j = i+1:N  % check only upper triangle
-            if A(i,j) ~= 0
-                edges = [edges; i, j];
-            end
-        end
-    end
+A(newNodeIndex, a) = 1;
+A(newNodeIndex, b) = 1;
 
-    % Chose a random edge
-    idx = randi(size(edges,1));
-    e = edges(idx, :);
-    a = e(1);
-    b = e(2);
+A(a, newNodeIndex) = 1;
+A(b, newNodeIndex) = 1;
 
-    A(newNodeIndex, a) = 1;
-    A(newNodeIndex, b) = 1;
+nodes{newNodeIndex}.shape = 's'; % Update the shape of the new node
 
-    A(a, newNodeIndex) = 1;
-    A(b, newNodeIndex) = 1;
-
-    nodes{newNodeIndex}.shape = 's'; % Update the shape of the new node
-          
 
 end
--- applyRule2.m
+++ applyRule2.m
@@ -1,54 +1,54 @@
 function [A, nodes] = applyRule2(A, nodes)
-    N = size(A,1);
+N = size(A,1);
 
-    % Find all unused node (circle node)
-    circleNodeIndexes = [];
-    for i = 1:N
-        if isequal(nodes{i}.shape, 'o')
-            circleNodeIndexes = [circleNodeIndexes; i];
+% Find all unused node (circle node)
+circleNodeIndexes = [];
+for i = 1:N
+    if isequal(nodes{i}.shape, 'o')
+        circleNodeIndexes = [circleNodeIndexes; i];
+    end
+end
+
+% Chose 1 random node
+if length(circleNodeIndexes) <=0
+    return; % Exit if no circle nodes are found
+end
+newNodeIndex = circleNodeIndexes(randi(length(circleNodeIndexes)));
+
+% Find all edge of A in from of (nodea, nodeb)
+edges = []; % Initialize empty list
+
+for i = 1:N
+    for j = i+1:N  % check only upper triangle
+        if A(i,j) ~= 0
+            edges = [edges; i, j];
         end
     end
-    
-    % Chose 1 random node
-    if length(circleNodeIndexes) <=0
-        return; % Exit if no circle nodes are found
-    end
-    newNodeIndex = circleNodeIndexes(randi(length(circleNodeIndexes)));
+end
 
-    % Find all edge of A in from of (nodea, nodeb)
-    edges = []; % Initialize empty list
+% Chose a random edge
+idx = randi(size(edges,1));
+e = edges(idx, :);
+a = e(1);
+b = e(2);
 
-    for i = 1:N
-        for j = i+1:N  % check only upper triangle
-            if A(i,j) ~= 0
-                edges = [edges; i, j];
-            end
-        end
-    end
+neighborsA = find(A(a,:) == 1);
+neighborsB = find(A(b,:) == 1);
+possibleC = unique([neighborsA neighborsB]);
+possibleC(possibleC == a | possibleC == b) = [];
 
-    % Chose a random edge
-    idx = randi(size(edges,1));
-    e = edges(idx, :);
-    a = e(1);
-    b = e(2);
+if isempty(possibleC)
+    return; % cannot apply H2
+end
+c = possibleC(randi(length(possibleC)));
 
-    neighborsA = find(A(a,:) == 1);
-    neighborsB = find(A(b,:) == 1);
-    possibleC = unique([neighborsA neighborsB]);
-    possibleC(possibleC == a | possibleC == b) = [];
-    
-    if isempty(possibleC)
-        return; % cannot apply H2
-    end
-    c = possibleC(randi(length(possibleC)));
-    
-    % Remove old edge
-    A(a,b) = 0; A(b,a) = 0;
-    
-    % Add correct Henneberg II edges:
-    A(newNodeIndex, a) = 1; A(a, newNodeIndex) = 1;
-    A(newNodeIndex, b) = 1; A(b, newNodeIndex) = 1;
-    A(newNodeIndex, c) = 1; A(c, newNodeIndex) = 1;
+% Remove old edge
+A(a,b) = 0; A(b,a) = 0;
 
-    nodes{newNodeIndex}.shape = 's';
+% Add correct Henneberg II edges:
+A(newNodeIndex, a) = 1; A(a, newNodeIndex) = 1;
+A(newNodeIndex, b) = 1; A(b, newNodeIndex) = 1;
+A(newNodeIndex, c) = 1; A(c, newNodeIndex) = 1;
+
+nodes{newNodeIndex}.shape = 's';
 end
